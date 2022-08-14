5e20e5f4be95f6cebaf777ee88cec0b1d6758f8d
Carlos Cruz <110253144+carloscruz01@users.noreply.github.com>
2022-08-14 15:31:09 -0300

    Add files via upload

---
+++ script_exercice_08.py
@@ -0,0 +1,10 @@
+nota1 = float(input('Entre com a nota:'))
+nota2 = float(input('Entre com a segunda nota:'))
+nota3 = float(input('Entre com a terceira nota:'))
+media = (nota1 + nota2 + nota3) / 3
+print("""A nota do primeiro bimestre: {}
+         Segundo Bimestre: {}
+         Terceiro Bimestre: {} equivalem a média total de: {}""".format(nota1,
+                                                                        nota2,
+                                                                        nota3,
+                                                                        media))
